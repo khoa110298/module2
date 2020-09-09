c6d0f6c71af3d29d5fb5ed74fbfd14e6879ecdc3
khoa110298 <nguyenkhoa15011998@gmail.com>
2020-09-09 08:10:49 +0700

    first commit

---
+++ src/bai1_introduction_to_java/thuc_hanh/HienThiThoiGianCuaHeThong.java
@@ -0,0 +1,9 @@
+package bai1_introduction_to_java.thuc_hanh;
+import java.util.Date;
+public class HienThiThoiGianCuaHeThong {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
