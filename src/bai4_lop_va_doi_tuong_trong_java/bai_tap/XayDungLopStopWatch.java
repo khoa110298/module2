8bcac7ae0ce49ed8df47a3f7757cf51aa4294814
khoa110298 <nguyenkhoa15011998@gmail.com>
2020-09-15 08:04:12 +0700

    up bai

--- src/bai4_lop_va_doi_tuong_trong_java/bai_tap/XayDungLopStopWatch.java
+++ src/bai4_lop_va_doi_tuong_trong_java/bai_tap/XayDungLopStopWatch.java
@@ -1,28 +1,44 @@
 package bai4_lop_va_doi_tuong_trong_java.bai_tap;
 
+import java.util.Arrays;
+
 public class XayDungLopStopWatch {
-//    long startTime;
-//    long endTime;
-//    public long getStartTime() {
-//        return startTime;
-//    }
-//    public long getEndTime() {
-//        return endTime;
-//    }
-//    public StopWatch() {
-//        this.startTime = System.currentTimeMillis();
-//    }
-//    public void start() {
-//        this.startTime = System.currentTimeMillis(); // 3h00
-//    }
-//    public void stop() {
-//        this.endTime = System.currentTimeMillis(); //3h15
-//    }
-//    public long getElapsedTime() {
-//        return this.endTime - this.startTime;
-//    }
-//
-//    public static void main(String[] args) {
-//        XayDungLopStopWatch stopWatch = new XayDungLopStopWatch();
-//    }
+
+    public static int[] arrayInt(){
+        int []arrayInt = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            arrayInt[i] = (int)(Math.random() * 100000);
+        }
+        return arrayInt;
+    }
+
+    public static void sapXep(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int maxIndex = i;
+            for (int j = i+1; j < n; j++){
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        int array[] = arrayInt();
+
+        sapXep(array);
+
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+    }
+
 }
