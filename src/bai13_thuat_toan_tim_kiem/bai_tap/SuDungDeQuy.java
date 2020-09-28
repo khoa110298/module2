4b553ce2af876f90e41967c4ad80fe4957f65e3e
khoa110298 <nguyenkhoa15011998@gmail.com>
2020-09-28 11:30:06 +0700

    up bai

---
+++ src/bai13_thuat_toan_tim_kiem/bai_tap/SuDungDeQuy.java
@@ -0,0 +1,28 @@
+package bai13_thuat_toan_tim_kiem.bai_tap;
+
+import java.util.Scanner;
+
+public class SuDungDeQuy {
+
+    static int binarySearch(int[] list, int low, int high, int key) {
+        if (high >= low) {
+            int mid = (high + low) / 2;
+            if (key == list[mid]) {
+                return mid;
+            } else if (key > list[mid]) {
+                return binarySearch(list, mid + 1, high, key);
+            } else {
+                return binarySearch(list, low, mid - 1, key);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhập số cần tìm kiếm: ");
+        int x = Integer.parseInt(sc.nextLine());
+        System.out.println(binarySearch(list, 0, list.length - 1, x));
+    }
+}
---
+++ src/bai13_thuat_toan_tim_kiem/thuc_hanh/BinarySearch.java
@@ -0,0 +1,31 @@
+package bai13_thuat_toan_tim_kiem.thuc_hanh;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (key > list[mid]) {
+                low = mid + 1;
+            } else if (key < list[mid]) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("nhập số cần tìm kiếm: ");
+            int x = Integer.parseInt(scanner.nextLine());
+            System.out.println(binarySearch(list, x));
+        }
+    }
+}
